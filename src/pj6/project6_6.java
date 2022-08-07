f63c547e9055f4a8e1b4e986ed4323e4745928eb
장유진 <wkddbwls145@gmail.com>
2022-08-07 11:33:02 +0900

    commit chapter 3

---
+++ src/pj6/project6_6.java
@@ -0,0 +1,41 @@
+package pj6;
+
+import java.util.Scanner;
+
+public class project6_6 {
+    static void shellSort(int[] a, int n){
+        for (int h = n /2; h>0; h /=2) {
+            for (int i=h;i<n;i++){
+                int j;
+                int tmp = a[i];
+                for(j=i-h; j >=0 && a[j] > tmp; j -=h){
+                    a[j+h] = a[j];
+                }
+                a[j+h] = tmp;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("단순 선택 정렬");
+        System.out.print("요솟수 : ");
+
+        int nx = stdIn.nextInt();
+        int[] x = new int[nx];
+
+        for (int i = 0; i < nx; i++) {
+            System.out.print("x["+i+"]: ");
+            x[i] = stdIn.nextInt();
+        }
+
+        shellSort(x,nx);
+
+        System.out.println("오름차순으로 정렬했습니다.");
+        for (int i = 0; i < nx; i++) {
+            System.out.println("x["+i+"]= "+x[i]);
+        }
+    }
+
+}
